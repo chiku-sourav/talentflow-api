68be5bffc45efd5fa638ed6d73b3a79a371b4770
Sourav Ghosh <sourav011990@gmail.com>
2026-01-26 15:46:04 +0530

    fix module imports

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService, PrismaService],
+})
 export class UsersModule {}
